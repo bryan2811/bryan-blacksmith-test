7b1b8ef21b9b1c82a906fae979e10e79604f41d6
Bryan Silva <bryan@leadboxhq.com>
2023-01-29 14:45:16 -0300

    Local environment setup

--- index.php
+++ index.php
@@ -20,5 +20,10 @@
 
         <?php include_once('template-parts/footer.php'); ?>
         <script type="text/javascript" src="dist/js/index.js" defer></script>
+        <script id="__bs_script__">
+        //<![CDATA[
+          document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.27.11'><\/script>".replace("HOST", location.hostname));
+        //]]>
+        </script>
     </body>
 </html>
